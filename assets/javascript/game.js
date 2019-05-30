22589b33d9166f8bbe6fdb1492683cbc87239eee
Stephanie Mattern <smattern416@gmail.com>
2019-05-30 08:18:44 -0400

    created outline comments of what to do

---
+++ assets/javascript/game.js
@@ -0,0 +1,14 @@
+var userGuess = prompt("Guess a letter!");
+
+// Computer prompts player
+// User inputs a letter 
+// Computer compares the users letter to their letter
+// Decide winner 
+// Allocate a point 
+// Update remaining guesses
+// Update how many guesses user has made so far 
+// Loop that whole thing
+// After however many guesses, game is over 
+// Alert player of who won or lost 
+
+ 

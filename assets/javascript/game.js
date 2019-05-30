a5e38f0e275506e52c28b0212e06df2b9433502d
Stephanie Mattern <smattern416@gmail.com>
2019-05-30 13:00:11 -0400

    threw in alphabet var

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,5 @@
 var userGuess = prompt("Guess a letter!");
-var alphabet = ["a", "b", ];
+var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var guesses = [ ];
 var wins ;
 var losses ;

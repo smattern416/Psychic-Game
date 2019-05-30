6d69fc8bfd8b83f003e3dee39f81b27a5d25e1ee
Stephanie Mattern <smattern416@gmail.com>
2019-05-30 16:56:10 -0400

    added some code to the js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,8 +1,25 @@
+
+alert("Yo what up");
+
 var userGuess = prompt("Guess a letter!");
 var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var guesses = [ ];
-var wins ;
-var losses ;
+var compGuess = alphabet[Math.floor(Math.random() * alphabet.length)];
+var wins = 0;
+var losses = 0;
+var ties = 0;
+var guessesLeft = 10;
+
+document.onkeyup = function(event) {
+    console.log("This is working");
+    var userGuess = event.key;
+    console.log(userGuess);
+
+
+
+}
+
+console.log(compGuess);
+
 
 
 

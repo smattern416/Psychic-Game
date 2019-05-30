5863af461daaf28aae0f25b1df6ff3d8d16fb3bf
Stephanie Mattern <smattern416@gmail.com>
2019-05-30 17:21:03 -0400

    adding in more var's to .js and added directions to html doc

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,13 +1,17 @@
 
-alert("Yo what up");
-
-var userGuess = prompt("Guess a letter!");
+var userGuess = document.getElementById("Guess a letter!");
 var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var compGuess = alphabet[Math.floor(Math.random() * alphabet.length)];
 var wins = 0;
 var losses = 0;
 var ties = 0;
 var guessesLeft = 10;
+var directionsText = document.getElementById("directions-text");
+    var userChoiceText = document.getElementById("userchoice-text");
+    var computerChoiceText = document.getElementById("computerchoice-text");
+    var winsText = document.getElementById("wins-text");
+    var lossesText = document.getElementById("losses-text");
+    var tiesText = document.getElementById("ties-text");
 
 document.onkeyup = function(event) {
     console.log("This is working");

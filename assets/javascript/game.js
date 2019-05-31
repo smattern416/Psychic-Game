9686c8690b2eec709f40a21c9c373f230237f599
Stephanie Mattern <smattern416@gmail.com>
2019-05-30 20:02:00 -0400

    added if elses

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,20 +1,52 @@
 
 var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var computerChoice = alphabet[randomIndex];
-var winCount = 1;
-var lossCount = 0;
+var compChoice = alphabet[randomIndex];
+var wins = 1;
+var losses = 0;
 var guessesRemaining = 10;
 var guessedLetters = [];
 var randomIndex = Math.floor(Math.random() * alphabet.length);
 
 
-document.onkeyup = function(event) {
+document.onkeyup = function (event) {
     var userChoice = event.key;
 
     var regexp = /[a-z]/gi;
     if (!regexp.test(userChoice)) {
-      alert("please enter a letter");
+        alert("Please enter a letter!");
     }
+    else {
+        console.log(userChoice);
+    }
+    if (guessesRemaining <= 0) {
+        losses++;
+        document.getElementById("losses").innerHTML = losses++;
+        guessesRemaining = 10;
+        guessedLetters = [];
+        document.getElementById("guessedLetters").innerHTML = guessedLetters;
+        document.getElementById("guessesRemaining").innerHTML = 10;
+        randomIndex = Math.floor(Math.random() * alphabet.length);
+        compChoice = alphabet[randomIndex];
+
+        if (compChoice === userChoice) {
+            document.getElementById("wins").innerHTML = wins++;
+            guessedLetters = [];
+            document.getElementById("guessedLetters").innerHTML = guessedLetters;
+            randomIndex = Math.floor(Math.random() * alphabet.length);
+            compChoice = alphabet[randomIndex];
+            console.log(compChoice);
+            guessesRemaining = 10;
+            document.getElementById("guessesRemaining").innerHTML = 10;
+        }
+        else {
+            document.getElementById("guessesRemaining").innerHTML = guessesRemaining--;
+            guessedLetters.push(userChoice);
+            document.getElementById("guessedLetters").innerHTML = guessedLetters;
+        }
+    }
+
+
+
 
 
 // Computer prompts player
